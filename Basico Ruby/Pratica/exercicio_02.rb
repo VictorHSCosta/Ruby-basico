657190638d315a354231f7a0a6fabdbce6996b65
VictorHSCosta <victorhenriquecosta23@gmail.com>
2025-04-19 14:37:22 -0300

    Add initial Ruby fundamentals documentation covering types, variables, and user input methods

---
+++ Basico Ruby/Pratica/exercicio_02.rb
@@ -0,0 +1,9 @@
+puts "Digite sua data de nascimento (dd/mm/aaaa):"
+data = gets
+
+puts "Data de nascimento: #{data}"
+
+# data = data.chomp
+puts "Digite novamente a sua data de nascimento (dd/mm/aaaa):"
+data = gets.chomp
+puts "Data de nascimento: #{data}"
