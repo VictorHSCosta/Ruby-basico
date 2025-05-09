fa510e0a5ea7083a619a36099f3aaefa63e1a03b
VictorHSCosta <victorhenriquecosta23@gmail.com>
2025-05-08 22:58:58 -0300

    Add search functionality to find occurrences of a word in text files

---
+++ Desafio search/search.rb
@@ -0,0 +1,32 @@
+def searchFile(word)
+  resposta = []
+
+  path = "/home/victor/Documentos/Estudo Ruby/ Basico Ruby 2/Pratica/Arquivos"
+    
+  Dir.glob("#{path}/*.txt") do |file_name|
+    readed_file = File.open(file_name,"r")
+
+    readed_file.each_with_index do |line, index|
+      line = line.downcase!
+      if line.include?(word)
+        resposta << [index + 1, file_name]
+      end
+    end
+
+    readed_file.close
+  end
+
+  resposta
+end
+
+word = ARGV[0].downcase!
+
+resposta = searchFile(word) if !word.nil?
+
+puts "Encontrado #{resposta.length} ocorrencias da palavra #{word}"
+
+if !resposta.empty?
+  resposta.each do |line|
+    puts "No arquivo #{line[1]} foi encontrado na linha #{line[0]}"
+  end
+end
